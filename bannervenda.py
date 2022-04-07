943072738e3ac896c9e790c4bcc21a11b52abe0f
Alan Garmatter <correa.ito@gmail.com>
2022-04-07 20:05:49 -0300

    first commit

---
+++ bannervenda.py
@@ -0,0 +1,2 @@
+class BannerVenda():
+    pass
---
+++ telas.py
@@ -0,0 +1,23 @@
+from kivy.uix.screenmanager import Screen
+
+
+class AjustesPage(Screen):
+    pass
+
+class HomePage(Screen):
+    pass
+
+class AdicionarVendasPage(Screen):
+    pass
+
+class ListarVendedoresPage(Screen):
+    pass
+
+class FotoPerfilPage(Screen):
+    pass
+
+class AdicionarVendedorPage(Screen):
+    pass
+
+class TodasVendasPage(Screen):
+    pass
